34ffc95d96ba83fd47131f106118b6efb2f12892
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2016-02-03 11:49:04 -0500

    Added Images and Fruits, added very generic Y axis movement for bugs, Fixed constants, Cleaned up code a bit
    Fruits:
    - Added spawn fruits functions
    - Need to add collision between fruits and Bugs
    - Spawn areas is a bit wonky, sometimes you’ll have a fruit spawn just
    barely off screen.
    - Sometimes (unlikely) fruit will spawn on top of one another or
    extreme close to one another
    
    Score and UI:
    - Presented a bit better, needs to be cleaned up more
    - Score updates per tick of ant creation, need to change to when ant is
    clicked
    
    Bugs:
    -Moves bugs down
    - Not removing any bugs that move off screen, will probably kill
    performance later on! Should change this
    - Bugs do not move towards fruit, need to implement ‘fruitFinder’
    function
    
    Misc:
    - Need to go over variable names and clean up code a bit
    - Documentation needs to be added

--- js/tapTapBug.js
+++ js/tapTapBug.js
@@ -1,36 +1,70 @@
-var TIMEPERLEVEL = 60;
-var score = 0;
-var topScore;
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
+
+const FPS = 45;
+
+var timeLeft = 60;
+var topScore;
+var score = 0;
+
+var blackSpeed = 150/FPS;
+var redSpeed = 75/FPS;
+var orangeSpeed = 60/FPS; 
+
 var bugs = [];
+var fruits = ["apple", "banana", "watermelon", "orange", "grape"];
+var Level = 1;
 
 function startGame() {
     document.getElementById("main").innerHTML = "<canvas id='game' width = '400' height = '600'>  </canvas> <audio id ='gameMusic' controls autoplay loop hidden: true;> <source src='audio/PlantsVsZombies.mp3' type='audio/mpeg'> </audio>";
 	canvas = document.getElementById("game");
 	context = canvas.getContext("2d");
+	initFruits();
 	createMenu();
-	
-	var myTimer = setInterval(runTimer, 1000);
+	beginTimers();
 
-	var timeToSpawn = randomize(1, 3);
-	var mainGameTimer = setInterval(playGame, 1000/25);
-	var spawnBugTimer = setInterval(spawnBug, timeToSpawn * 1000);
 	//var audio = document.getElementById("gameMusic");
     //audio.remove(audio);
 }
+
 function createMenu () {
 	context.globalAlpha=0.2;
 	context.fillStyle="blue"; 
 	context.fillRect(0,0,400,50);
+	context.font = "18px Arial";
+	context.fillText("Score: " + score, 310, 35);
+	context.font = "18px Arial";
+	context.fillText("Time Left: " + timeLeft, 10, 35);
 }
 
-function playGame() {
 
+function playGame() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	moveBugs();
+	createMenu();
+	spawnFruits();
+
+}
+
+function beginTimers() {
+	var timeToSpawn = randomize(1, 3);
+	
+	var countdownTimer = setInterval(runTimer, 1000);
+	var mainGameTimer = setInterval(playGame, FPS);
+	var addBugTimer = setInterval(addBug, timeToSpawn * 1000);
+}
+
+function pauseGame() {
+	window.clearTimeout(mainGameTimer);
+	window.clearTimeout(addBugTimer);
+	window.clearTimeout(countdownTimer);
 }
 
 function moveBugs() {
-
+	for (var i = 0; i < bugs.length; i++) {
+		bugs[i][4] = bugs[i][4] + bugs[i][1];
+		makeBug(context, bugs[i][0], 0.5, bugs[i][3], bugs[i][4]);
+	}
 }
 
 function randomize(lowest, highest) {
@@ -38,34 +72,60 @@ function randomize(lowest, highest) {
 	return number;
 }
 
-function spawnBug() {
+function addBug() {
 	var whereToSpawn = randomize(10, 390);
 	var colourOfAnt = randomize(1, 100);
+	var speed;
+	var points;
 
 	if (colourOfAnt < 30) {
-		colourOfAnt = "black"
+		colourOfAnt = "#161616";
+		points = 5;
+		speed = blackSpeed;
 	} else if (colourOfAnt < 60) {
-		colourOfAnt = "red"
+		colourOfAnt = "#C22121";
+		points = 3;
+		speed = redSpeed;
 	} else {
-		colourOfAnt = "orange"
+		colourOfAnt = "#F57336";
+		points = 1;
+		speed = orangeSpeed;
 	}
 
-	makeBug(context, colourOfAnt, 0.5, whereToSpawn, 70);
+	//Make an Array for the bug which has: colour, speed, visibility, points, x position, y position
 
+	bugs.push([colourOfAnt, speed, points, whereToSpawn, 50]);
 	//Cookie Testing:
 	score += 1;
-	context.font = "18px Arial";
-	context.fillText(score, 50, 35);
 	setScore();
+}
 
+function initFruits() {
+	var xPosition;
+	var yPosition;
 
+	for (var i = 0; i < 5; i++) {
+		xPosition = randomize(10, 350);
+		yPosition = randomize(150, 530);
+		var image = document.getElementById(fruits[i]);
+
+		//Replace fruit name in array with array detailing image name, alpha/visibility, xPosition and yPosition
+		fruits[i] = [image, xPosition, yPosition];
+		
+	}
+}
+
+function spawnFruits() {
+	context.globalAlpha = 1;
+	for (var i = 0; i < fruits.length; i++) {
+		context.drawImage(fruits[i][0], fruits[i][1], fruits[i][2], 30, 30);
+	} 
 }
 
 function runTimer() {
-	TIMEPERLEVEL--;
-	context.font = "18px Arial";
-	context.fillText(TIMEPERLEVEL, 10, 35);
+	timeLeft--;
 }
+
 /**
 * On load, html page will fetch the stored score
 **/
