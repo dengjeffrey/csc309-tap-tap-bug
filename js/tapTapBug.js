3de0ef02cb2fb40a8a94f5897fbe28ab50e70aa9
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2016-02-08 16:32:23 -0500

    Bugs can now eat fruits, game crashes after all fruits eaten though
    Bugs can eat fruits! Must add game over screen/next level screen
    though!

--- js/tapTapBug.js
+++ js/tapTapBug.js
@@ -251,28 +251,35 @@ function moveBugs() {
 		var xTranslation;
 		var yTranslation;
 		var fruitNumber = shortestDistance(i);
-		var x1 = bugs[i][3];
-		var y1 = bugs[i][4];
-		var x2 = fruits[fruitNumber][1] + 6;
-		var y2 = fruits[fruitNumber][2];
+		//Initialize the four approximate corners of the Bug
+		var bL = bugs[i][3];
+		var bT = bugs[i][4];
+		var bR = bugs[i][5];
+		var bB = bugs[i][6];
 
-		if (x1 == x2) {
+		//Approximate the four corners of the Fruit
+		var fL = fruits[fruitNumber][1] + 6;
+		var fR = fL + 24;
+		var fT = fruits[fruitNumber][2];
+		var fB = fT + 30;
+
+		if (bL == fL) {
 			xTranslation = 0;
 			yTranslation = bugs[i][1];
-		} else if (y1 == y2) {
+		} else if (bT == fT) {
 			yTranslation = 0;
 			xTranslation = bugs[i][1];
 
-			if (x2 < x1) {
+			if (fL < bL) {
 				xTranslation = -xTranslation;
 			}
 		} else {
-			var distanceAngle = Math.atan((x2-x1)/(y2-y1));
+			var distanceAngle = Math.atan((fL-bL)/(fT-bT));
 			xTranslation = bugs[i][1] * Math.sin(distanceAngle);
 			yTranslation = bugs[i][1] * Math.cos(distanceAngle);
 		}
 
-		if (y2 < y1) {
+		if (fT < bT) {
 			yTranslation = -yTranslation;
 		}
 
@@ -280,6 +287,16 @@ function moveBugs() {
 		bugs[i][4] = bugs[i][4] + yTranslation;
 
 		makeBug(context, bugs[i][0], 0.5, bugs[i][3], bugs[i][4]);
+
+		/** 
+		*Check if any of the bugs are touching any of the fruits. If any of the points of the bug is in between 
+		**/
+
+		if ((bL <= fR && bL >= fL) || (bR >= fL && bR <= fR)) {
+			if ((bT >= fT && bT <= fB) || (bB >= fT && bB <= fB)) {
+				fruits.splice(fruitNumber, 1);
+			}
+		}
 	}
 }
 
@@ -309,8 +326,8 @@ function addBug() {
 		speed = orangeSpeed/FPS;
 	}
 
-	//Make an Array for the bug which has: colour, speed, points, x position, y position
-	bugs.push([colourOfAnt, speed, points, whereToSpawn, 50]);
+	//Make an Array for the bug which has: colour, speed, points, x position, y position, right Limit, bottom Limit
+	bugs.push([colourOfAnt, speed, points, whereToSpawn, 50, whereToSpawn + 24, 50 + 79]);
 	//Cookie Testing:
 	score += 1;
 	setScore();
@@ -435,6 +452,12 @@ function mouseDidPressDown(event) {
 	}
 	
 }
+
+function checkCollision(x1, y1, x2, y2) {
+
+
+}
+
 
 function mouseDidRelease(event) {
 	
