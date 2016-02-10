46607f1fa7da56a867d7c89f2f468b280332e04a
Jeffrey Deng <jeffreydeng@live.com>
2016-02-10 18:34:51 -0500

    Added our custom bug drawings

--- js/bug.js
+++ js/bug.js
@@ -1,75 +1,128 @@
+const HEIGHT = 50;
+const HEAD_PROPORTION = 0.35;
+const BODY_PROPORTION = 0.3;
+const END_PROPORTION = 0.4;
+// Where each portion of the body begins in percentage of the height of the last body part
+const BODY_CONNECTION_PROPORTION_INSET = 0.3;
+// Distance between the two bottom endpoints of the atennas 
+const ATENNA_STEEPNESS = 3;
+const PINCER_SPACING = 3;
+const PINCER_HEIGHT = 5;
+// Legs
+// Where the leg starts n proportion to the height of the middle proportion
+const FIRST_LEG_START_PROPORTION = 0.46;
+const SECOND_LEG_START_PROPORTION = 0.75;
+const LAST_LEG_START_PROPORTION = 0.65;
+// Where the limb starts n proportion to the height of the whole ant
+const FIRST_LEG_LIMB_PROPORTION = 0.45;
+const SECOND_LEG_LIMB_PROPORTION = 0.58;
+const LAST_LEG_LIMB_PROPORTION = 0.75;
+const FIRST_LEG_END_PROPORTION = 0.50;
+const SECOND_LEG_END_PROPORTION = 0.70;
+const LAST_LEG_END_PROPORTION = 0.94;
+const FIRST_LEG_END_WIDTH_PROPORTION = 0.12;
+const SECOND_LEG_END_WIDTH_PROPORTION = 0.14;
+const LAST_LEG_END_WIDTH_PROPORTION = 0.08;
+
 // Painting one bug with x, y being left top corner
-    function makeBug(context, colour, visibility, x, y){
-        
-        color = colour;
-        alpha = visibility;
-        
-        //http://www.w3schools.com/tags/canvas_globalalpha.asp
-        context.globalAlpha = alpha;
-        
-        /*-- Whiskers, legs and arms--*/
-        context.beginPath();
-        context.moveTo(x,y);
-        context.lineTo(x+5, y+15);
-        context.lineTo(x+10, y);
-        context.moveTo(x+5, y+20);
-        context.lineTo(x+4, y+22);
-        context.lineTo(x+6, y+22);
-        context.lineTo(x+5, y+20);
-        context.moveTo(x, y+20);
-        context.lineTo(x+10, y+40);
-        context.moveTo(x+10, y+20);
-        context.lineTo(x, y+40);
-        context.lineWidth = 2;
-        context.strokeStyle = color;
+function makeBug(ctx, colour, visibility, x, y) {
+	const WIDTH = HEIGHT * 0.65;
+	var xCenter = WIDTH / 2 + x;
+	var yBottom = HEIGHT + y;
+	var headRadius = (HEIGHT * HEAD_PROPORTION / 2);
+	var middleRadius = (HEIGHT * BODY_PROPORTION / 2);
+	var endRadius = (HEIGHT * END_PROPORTION / 2);
 
-        /*-- Triangles on the tips --*/
-        context.moveTo(x,y);
-        context.lineTo(x, y+3);
-        context.lineTo(x+1.73, y+2.4);
-        context.lineTo(x, y);
-        context.moveTo(x+10, y);
-        context.lineTo(x+8.27, y+2.4);
-        context.lineTo(x+10, y+3);
-        context.lineTo(x+10, y);
-        context.moveTo(x, y+20);
-        context.lineTo(x, y+22);
-        context.lineTo(x+1.6, y+21.25);
-        context.lineTo(x, y+22);
-        context.moveTo(x+10, y+20);
-        context.lineTo(x+8.4, y+21.25);
-        context.lineTo(x+10, y+22);
-        context.lineTo(x+10, y+20);
-        context.moveTo(x, y+40);
-        context.lineTo(x, y+38);
-        context.lineTo(x+1.6, y+38.25);
-        context.lineTo(x, y+38);
-        context.moveTo(x+10, y+40);
-        context.lineTo(x+8.4, y+38.25);
-        context.lineTo(x+10, y+38);
-        context.lineTo(x+10, y+40);
-        context.stroke();
-
-        /*-- Body parts --*/
-        context.beginPath();
-        context.arc(x+5, y+15, 5, 0, 2*Math.PI);
-        context.moveTo(x+5, y+21);
-        context.bezierCurveTo(x, y+20, x, y+30, x+5, y+38.75);
-        context.moveTo(x+5, y+21);	
-        context.bezierCurveTo(x+10, y+20, x+10, y+30, x+5, y+38.75);
-        context.fillStyle = color;
-        context.lineWidth = 1;
-        context.strokeStyle = "#000000"
-        context.stroke();
-        context.fill();
-
-        /*-- Eyes and Mouth --*/
-        context.beginPath();
-        context.arc(x+3.3, y+13.2, 1, 0, 2*Math.PI);
-        context.arc(x+6.75, y+13.2, 1, 0, 2*Math.PI);
-        context.fillStyle = "white";
-        context.fill();
-        context.beginPath();
-        context.arc(x+5, y+15, 2.5, 0, Math.PI, false);
-        context.stroke();
-    }
+	ctx.globalAlpha = 1;
+	// Bottom
+	ctx.beginPath();
+	var endBodyCenterY = yBottom - endRadius;
+	var endBodyBottomY = endBodyCenterY + endRadius;
+	ctx.arc(xCenter, endBodyCenterY, endRadius, 0, 2 * Math.PI, false);
+	ctx.fillStyle = colour;
+	ctx.fill();
+	// Middle
+	ctx.beginPath();
+	var middleBodyBottomY = (endBodyCenterY - endRadius) + BODY_CONNECTION_PROPORTION_INSET * (endRadius * 2);
+	var middleBodyCenterY = middleBodyBottomY - middleRadius;
+	ctx.arc(xCenter, middleBodyCenterY, middleRadius, 0, 2 * Math.PI, false);
+	ctx.fillStyle = colour;
+	ctx.fill();
+	// Head
+	ctx.beginPath();
+	var headBodyBottomY = (middleBodyCenterY - middleRadius) + BODY_CONNECTION_PROPORTION_INSET * (middleRadius * 2);
+	var headBodyCenterY = headBodyBottomY - headRadius;
+	ctx.arc(xCenter, headBodyCenterY, headRadius, 0, 2 * Math.PI, false);
+	ctx.fillStyle = colour;
+	ctx.fill();
+	// Atennas
+	ctx.beginPath();
+	ctx.strokeStyle = colour;
+	ctx.lineWidth = 1;
+	ctx.moveTo(xCenter - endRadius, y);
+	ctx.lineTo(xCenter - ATENNA_STEEPNESS / 2, headBodyCenterY);
+	ctx.stroke();
+	ctx.moveTo(xCenter + endRadius, y);
+	ctx.lineTo(xCenter + ATENNA_STEEPNESS / 2, headBodyCenterY);
+	ctx.stroke();
+	// Pincers
+	// Left
+	ctx.beginPath();
+	ctx.fillStyle = colour;
+	ctx.moveTo(xCenter - PINCER_SPACING / 2, headBodyCenterY - headRadius - PINCER_HEIGHT);
+	ctx.lineTo(xCenter - headRadius, headBodyCenterY);
+	ctx.lineTo(xCenter - PINCER_SPACING / 2, headBodyCenterY);
+	ctx.fill();
+	// Right
+	ctx.beginPath();
+	ctx.fillStyle = colour;
+	ctx.moveTo(xCenter + PINCER_SPACING / 2, headBodyCenterY - headRadius - PINCER_HEIGHT);
+	ctx.lineTo(xCenter + headRadius, headBodyCenterY);
+	ctx.lineTo(xCenter + PINCER_SPACING / 2, headBodyCenterY);
+	ctx.fill();
+	// Legs
+	// Left
+	var firstLegStartingY = middleBodyBottomY - (middleRadius * 2) * (1 - FIRST_LEG_START_PROPORTION);
+	ctx.beginPath();
+	ctx.moveTo(xCenter, firstLegStartingY);
+	ctx.lineTo(x + WIDTH * FIRST_LEG_END_WIDTH_PROPORTION, y + HEIGHT * FIRST_LEG_LIMB_PROPORTION);
+	ctx.lineTo(x, y + HEIGHT * FIRST_LEG_END_PROPORTION);
+	ctx.stroke();
+	var secondLegStartingY = middleBodyBottomY - (middleRadius * 2) * (1 - SECOND_LEG_START_PROPORTION);
+	ctx.beginPath();
+	ctx.moveTo(xCenter, secondLegStartingY);
+	ctx.lineTo(x + WIDTH * SECOND_LEG_END_WIDTH_PROPORTION, y + HEIGHT * SECOND_LEG_LIMB_PROPORTION);
+	ctx.lineTo(x, y + HEIGHT * SECOND_LEG_END_PROPORTION);
+	ctx.stroke();
+	var thirdLegStartingY = middleBodyBottomY - (middleRadius * 2) * (1 - LAST_LEG_START_PROPORTION);
+	ctx.beginPath();
+	ctx.moveTo(xCenter, thirdLegStartingY);
+	ctx.lineTo(x + WIDTH * LAST_LEG_END_WIDTH_PROPORTION, y + HEIGHT * LAST_LEG_LIMB_PROPORTION);
+	ctx.lineTo(x, y + HEIGHT * LAST_LEG_END_PROPORTION);
+	ctx.stroke();
+	// Legs
+	// Right
+	var firstLegStartingY = middleBodyBottomY - (middleRadius * 2) * (1 - FIRST_LEG_START_PROPORTION);
+	ctx.beginPath();
+	ctx.moveTo(xCenter, firstLegStartingY);
+	ctx.lineTo((x + WIDTH) - WIDTH * FIRST_LEG_END_WIDTH_PROPORTION, y + HEIGHT * FIRST_LEG_LIMB_PROPORTION);
+	ctx.lineTo((x + WIDTH), y + HEIGHT * FIRST_LEG_END_PROPORTION);
+	ctx.stroke();
+	var secondLegStartingY = middleBodyBottomY - (middleRadius * 2) * (1 - SECOND_LEG_START_PROPORTION);
+	ctx.beginPath();
+	ctx.moveTo(xCenter, secondLegStartingY);
+	ctx.lineTo((x + WIDTH) - WIDTH * SECOND_LEG_END_WIDTH_PROPORTION, y + HEIGHT * SECOND_LEG_LIMB_PROPORTION);
+	ctx.lineTo(x + WIDTH, y + HEIGHT * SECOND_LEG_END_PROPORTION);
+	ctx.stroke();
+	var thirdLegStartingY = middleBodyBottomY - (middleRadius * 2) * (1 - LAST_LEG_START_PROPORTION);
+	ctx.beginPath();
+	ctx.moveTo(xCenter, thirdLegStartingY);
+	ctx.lineTo((x + WIDTH) - WIDTH * LAST_LEG_END_WIDTH_PROPORTION, y + HEIGHT * LAST_LEG_LIMB_PROPORTION);
+	ctx.lineTo(x + WIDTH, y + HEIGHT * LAST_LEG_END_PROPORTION);
+	ctx.stroke();
+	// White overlay
+	ctx.beginPath();
+	ctx.globalAlpha = visibility;
+	ctx.fillStyle = "white"
+	ctx.fillRect(x, y, WIDTH, HEIGHT);
+}
